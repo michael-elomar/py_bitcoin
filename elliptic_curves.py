b5813fa05070f2b5a638a323f1df6b9597e66f92
Michael El Omar <quisutdeus100@gmail.com>
2022-11-29 23:43:46 +0100

    class Point that covers Point creation and addition on elliptic curves of form : y^2 = x^3 + a*x + b

---
+++ elliptic_curves.py
@@ -0,0 +1,51 @@
+class Point:
+    def __init__(self, x, y, a, b):
+        self.x = x
+        self.y = y
+        self.a = a
+        self.b = b
+        if self.x is None and self.y is None:
+            return
+        if self.y**2 != self.x**3 + self.a*self.x + b:
+            error_msg = f'Point ({self.x},{self.y}) is not on the elliptic curve'
+            error_msg = f'{error_msg} defined by y^2 = x^3 + a*x + b'
+            raise ValueError(error_msg)
+
+    def __repr__(self):
+        return f"Point_[{self.a},{self.b}]({self.x}, {self.y})"
+
+    def _same_curve(self, other):
+        return self.a == other.a and self.b == other.b
+
+    def __eq__(self, other):
+        assert type(other) == Point
+        return self.a == other.a and self.b == other.b \
+            and self.x == other.x and self.y == other.y
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __add__(self, other):
+        if type(other) is not self.__class__:
+            raise TypeError(f"{other} is of type : {type(other)} and  not {self.__class__}")
+        if not self._same_curve(other):
+            raise TypeError(f"Points {self}, {other} are not on the same curve")
+        if self.x is None:
+            return other
+        if other.x is None:
+            return self
+
+        if self.x == other.x and self.y != other.y:
+            return self.__class__(None, None, self.a, self.b)
+        if self.x != other.x:
+            s = (other.y - self.y) / (other.x - self.x)
+            x3 = s**2 - self.x - other.x
+            y3 = s*(self.x - x3) - self.y
+            return self.__class__(x3, y3, self.a, self.b)
+        if self.x == other.x and self.y == other.y:
+            s = (3*self.x**2 + self.a) / (2*self.y)
+            x3 = s**2 - 2*self.x
+            y3 = s*(self.x - x3) - self.y
+            return self.__class__(x3, y3, self.a, self.b)
+        if self == other and self.y == 0:
+            return self.__class__(None, None, self.a, self.b)

04f6a01c46cb1f4686111e5c17f0e3a314cba6a3
Michael El Omar <quisutdeus100@gmail.com>
2023-09-27 12:59:06 +0200

    add docstrings and new testcase

--- tests/elliptic_curves_test.py
+++ tests/elliptic_curves_test.py
@@ -1,5 +1,5 @@
 from tests.generic_test import GenericTest
-from bitcoin import Point, FieldElement
+from bitcoin import Point, FieldElement, S256Point, Signature
 
 import unittest
 
@@ -18,7 +18,7 @@ class PointTest(GenericTest):
         self.assertNotEqual(p1, p2)
         self.assertEqual(p1 + p2, p3)
         self.assertEqual(p2 + p2, p4)
-        self.logger.info('Point test passed !')
+        self.logger.info('Point test passed!')
 
 
 class FiniteFieldPointTest(GenericTest):
@@ -39,7 +39,7 @@ class FiniteFieldPointTest(GenericTest):
             y = FieldElement(y, prime)
             p = Point(x, y, a, b)
             self.assertTrue(p._is_on_curve(a, b))
-        self.logger.info("On Curve Test Passed")
+        self.logger.info("On Curve Test Passed!")
 
     def test_not_on_curve(self):
         prime = 223
@@ -55,9 +55,9 @@ class FiniteFieldPointTest(GenericTest):
             with self.assertRaises(ValueError):
                 Point(x, y, a, b)
 
-        self.logger.info("Not on curve test passed")
+        self.logger.info("Not on curve test passed!")
 
-    def test_elliptic_curve_addition(self):
+    def test_add(self):
         a = FieldElement(0, 223)
         b = FieldElement(7, 223)
 
@@ -75,8 +75,43 @@ class FiniteFieldPointTest(GenericTest):
         p3 = Point(x3, y3, a, b)
 
         self.assertEqual(p1 + p2, p3)
-        self.logger.info("test_elliptic_curve_addition passed")
 
+        x1 = FieldElement(170, 223)
+        y1 = FieldElement(142, 223)
+
+        x2 = FieldElement(num=60, prime=223)
+        y2 = FieldElement(num=139, prime=223)
+
+        x3 = FieldElement(220, 223)
+        y3 = FieldElement(181, 223)
+
+        p1 = Point(x1, y1, a, b)
+        p2 = Point(x2, y2, a, b)
+        p3 = Point(x3, y3, a, b)
+        self.assertEqual(p1 + p2, p3)
+        self.logger.info("Elliptic curve addition test passed!")
+
+    def test_valid_signature(self):
+        P = S256Point(
+            x=0x887387e452b8eacc4acfde10d9aaf7f6d9a0f975aabb10d006e4da568744d06c,
+            y=0x61de6d95231cd89026e286df3b6ae4a894a3378e393e93a0f45b666329a0ae34
+        )
+        z1 = 0xec208baa0fc1c19f708a9ca96fdeff3ac3f230bb4a7ba4aede4942ad003c0f60
+        sig1 = Signature(
+            r=0xac8d1c87e51d0d441be8b3dd5b05c8795b48875dffe00b7ffcfac23010d3a395,
+            s=0x68342ceff8935ededd102dd876ffd6ba72d6a427a3edb13d26eb0781cb423c4
+        )
+        self.assertTrue(P.verify(z1, sig1))
+
+        z2 = 0x7c076ff316692a3d7eb3c3bb0f8b1488cf72e1afcd929e29307032997a838a3d
+        sig2 = Signature(
+            r = 0xeff69ef2b1bd93a66ed5219add4fb51e11a840f404876325a1e8ffe0529a2c,
+            s = 0xc7207fee197d27c618aea621406f6bf5ef6fca38681d82b2f06fddbdce6feab6
+        )
+        self.assertTrue(P.verify(z2, sig2))
+
+
+        self.logger.info("Signature verification test passed!")
 
 if __name__ == '__main__':
     unittest.main()
